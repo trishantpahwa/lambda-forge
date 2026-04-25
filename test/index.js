3ef11c3132772a6f135a52f849f3bd0730ee58dd
Trishant Pahwa <trishantpahwa@gmail.com>
2026-04-26 02:28:53 +0530

    feat: initialize project with package.json and basic test setup
    - Added package.json for project metadata and dependencies
    - Included Mocha as a development dependency for testing
    - Created initial test file with a placeholder test case

---
+++ test/index.js
@@ -0,0 +1,8 @@
+const { describe, it } = require('mocha');
+
+describe('Lambda Forge', () => {
+    it('should have tests', () => {
+        // TODO: Add tests
+        console.log('Tests not implemented yet');
+    });
+});
